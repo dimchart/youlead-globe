cf52c2007e9310f396e0b93961460524d74f1cbd
Dima <316662426+dimchart@users.noreply.github.com>
2026-09-23 23:46:05 +0200

    Update members.js with new member entries and Telegram handles

--- members.js
+++ members.js
@@ -6,27 +6,271 @@ window.MEMBERS = [
     "role": "YouLEAD 2025",
     "city": "Київ",
     "lat": 50.4501,
-    "lng": 30.5234
+    "lng": 30.5234,
+    "telegram": "nn1me"
   },
   {
     "name": "Daria Levadna",
     "role": "YouLEAD 2025",
     "city": "Дніпро",
     "lat": 48.4647,
-    "lng": 35.0462
+    "lng": 35.0462,
+    "telegram": "dar_levadna"
   },
   {
     "name": "Alisa Shcherbak",
     "role": "YouLEAD 2025",
     "city": "Київ",
     "lat": 50.4501,
-    "lng": 30.5234
+    "lng": 30.5234,
+    "telegram": "ailiisaa"
   },
   {
     "name": "Anastasiia Batechko",
     "role": "YouLEAD 2025",
     "city": "Буча",
     "lat": 50.5436,
-    "lng": 30.2139
+    "lng": 30.2139,
+    "telegram": "Naseqw"
+  },
+  {
+    "name": "Pavlo Aizenberh",
+    "role": "YouLEAD 2025",
+    "city": "Львів",
+    "lat": 49.8397,
+    "lng": 24.0297,
+    "telegram": "eiissenberg"
+  },
+  {
+    "name": "Anna Bielik",
+    "role": "YouLEAD 2026",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "annxq"
+  },
+  {
+    "name": "Kirill Horkov",
+    "role": "YouLEAD 2026",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "kirillhorkov"
+  },
+  {
+    "name": "Stefaniia Durytska",
+    "role": "YouLEAD 2025",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "streemodie"
+  },
+  {
+    "name": "Anya Mishchuk",
+    "role": "YouLEAD 2026",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "anyamishaa"
+  },
+  {
+    "name": "Yeva Mohylevska",
+    "role": "YouLEAD 2026",
+    "city": "Хмельницький",
+    "lat": 49.423,
+    "lng": 26.9871,
+    "telegram": "qrevww"
+  },
+  {
+    "name": "Veronika Bondarenko",
+    "role": "YouLEAD 2026",
+    "city": "Аліканте",
+    "lat": 38.3452,
+    "lng": -0.481,
+    "telegram": "lll1va"
+  },
+  {
+    "name": "Maksym Fedorovskyi",
+    "role": "YouLEAD 2026",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "Maksymshark"
+  },
+  {
+    "name": "Oleksandr Serko",
+    "role": "YouLEAD 2026",
+    "city": "Ковель",
+    "lat": 51.2167,
+    "lng": 24.7167,
+    "telegram": "santyorik"
+  },
+  {
+    "name": "Khrystyna Shpyrka",
+    "role": "YouLEAD 2025",
+    "city": "Відень",
+    "lat": 48.2082,
+    "lng": 16.3738,
+    "telegram": "shpyrkaa"
+  },
+  {
+    "name": "Liza Zolotarova",
+    "role": "YouLEAD 2025",
+    "city": "Золотурн",
+    "lat": 47.2088,
+    "lng": 7.5372,
+    "telegram": "rkrx_li"
+  },
+  {
+    "name": "Mariia Bilytska",
+    "role": "YouLEAD 2025",
+    "city": "Вінниця",
+    "lat": 49.2331,
+    "lng": 28.4682,
+    "telegram": "yesimmeri"
+  },
+  {
+    "name": "Sofiko Ponomarova",
+    "role": "YouLEAD 2026",
+    "city": "Харків",
+    "lat": 49.9935,
+    "lng": 36.2304,
+    "telegram": "sowwks"
+  },
+  {
+    "name": "Maria Fedorchuk",
+    "role": "YouLEAD 2025",
+    "city": "Хмельницький",
+    "lat": 49.423,
+    "lng": 26.9871,
+    "telegram": "maskamariyka"
+  },
+  {
+    "name": "Anna Vasylieva",
+    "role": "YouLEAD 2024",
+    "city": "Ірпінь",
+    "lat": 50.5218,
+    "lng": 30.2506,
+    "telegram": "srgvnna"
+  },
+  {
+    "name": "Dmytro Baran",
+    "role": "YouLEAD 2024",
+    "city": "Варшава",
+    "lat": 52.2297,
+    "lng": 21.0122,
+    "telegram": "dim4art"
+  },
+  {
+    "name": "Anastasiia Lavrenchuk",
+    "role": "YouLEAD 2024",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "sargeryy"
+  },
+  {
+    "name": "Arina Bielik",
+    "role": "YouLEAD 2024",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "arinabielik"
+  },
+  {
+    "name": "Mykhailo Dyshkant",
+    "role": "YouLEAD 2024",
+    "city": "Братислава",
+    "lat": 48.1486,
+    "lng": 17.1077,
+    "telegram": "pandonn1o"
+  },
+  {
+    "name": "Yuliia Tertyshnyk",
+    "role": "YouLEAD 2026",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "julkiiinss"
+  },
+  {
+    "name": "Artem Serebriakov",
+    "role": "YouLEAD 2024",
+    "city": "Гельсінкі",
+    "lat": 60.1699,
+    "lng": 24.9384,
+    "telegram": "artemimus"
+  },
+  {
+    "name": "Arzy Adzhibekirova",
+    "role": "YouLEAD 2024",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "ArzyAdzhi"
+  },
+  {
+    "name": "Diana Bulai",
+    "role": "YouLEAD 2025",
+    "city": "Світловодськ",
+    "lat": 49.0483,
+    "lng": 33.2433,
+    "telegram": "dianabulai"
+  },
+  {
+    "name": "Kateryna Mysko",
+    "role": "YouLEAD 2026",
+    "city": "Київ",
+    "lat": 50.4501,
+    "lng": 30.5234,
+    "telegram": "katrinneesss"
+  },
+  {
+    "name": "Oleksandr Kachkan",
+    "role": "YouLEAD 2025",
+    "city": "Львів",
+    "lat": 49.8397,
+    "lng": 24.0297,
+    "telegram": "San3X_x"
+  },
+  {
+    "name": "Maksym Zahorodnii",
+    "role": "YouLEAD 2025",
+    "city": "Бориспіль",
+    "lat": 50.3528,
+    "lng": 30.9553,
+    "telegram": "MaximZagorodnii"
+  },
+  {
+    "name": "Mariia Hryhurko",
+    "role": "YouLEAD 2024",
+    "city": "Одеса",
+    "lat": 46.4825,
+    "lng": 30.7233,
+    "telegram": "marifxn"
+  },
+  {
+    "name": "Anastasiia Dabizha",
+    "role": "YouLEAD 2024",
+    "city": "Одеса",
+    "lat": 46.4825,
+    "lng": 30.7233,
+    "telegram": "nstsaa_a"
+  },
+  {
+    "name": "Mykola Vaskiv",
+    "role": "YouLEAD 2026",
+    "city": "Львів",
+    "lat": 49.8397,
+    "lng": 24.0297,
+    "telegram": "mykola_waskiw"
+  },
+  {
+    "name": "Anton Mazharystyi",
+    "role": "YouLEAD 2022",
+    "city": "Братислава",
+    "lat": 48.1486,
+    "lng": 17.1077,
+    "telegram": "yntytyn"
   }
 ];
